327bb8e9c64c740a33d89af0b4e33b2db593d94b
Mehak <mehakmattoo7@gmail.com>
2024-06-21 18:34:50 +0530

    added login & signup button

--- src/app/(home)/components/Navbar/Navbar.tsx
+++ src/app/(home)/components/Navbar/Navbar.tsx
@@ -4,9 +4,9 @@ import "./Navbar.css";
 const Navbar: React.FC = () => {
   return (
     <nav className="nav-container p-4">
-      <div className="container mx-auto flex  items-center">
-        <div className="text-white font-bold text-xl mr-5 ">Bingo</div>
-        <ul className="flex space-x-4 ">
+      <div className="container mx-auto flex items-center">
+        <div className="text-white font-bold text-xl mr-4">Bingo</div>
+        <ul className="flex space-x-4">
           <li>
             <a href="/" className="text-white hover:text-gray-200">
               Home
@@ -23,6 +23,25 @@ const Navbar: React.FC = () => {
             </a>
           </li>
         </ul>
+        <div className="ml-auto">
+          <button className="button">
+            <a
+              href="/login"
+              className=" pt-2 px-3 pb-2 rounded-xl  hover:bg-orange-400 mr-4"
+            >
+              Log In
+            </a>
+          </button>
+
+          <button className="button">
+            <a
+              href="/signup"
+              className=" pt-2 px-3 pb-2 rounded-xl   hover:bg-orange-400  mr-4"
+            >
+              Sign Up
+            </a>
+          </button>
+        </div>
       </div>
     </nav>
   );
